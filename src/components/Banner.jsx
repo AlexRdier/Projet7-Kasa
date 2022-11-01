153fd2d0584b171a7594463d4bd928e0ea74c1fa
Alex Roudier <alexroudier96@gmail.com>
2022-11-01 13:01:49 +0100

    ajout components + sass

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = ({ location }) => {
+  return (
+    <div className={"banner_container" + location}>
+      <p>
+        Chez vous, <span>partout et ailleurs</span>
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../img/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="nav_container">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="links">
+          <nav>
+            <ul>
+              <NavLink
+                to="/"
+                className={(nav) => (nav.isActive ? "nav-active" : "")}
+                end
+              >
+                <li>Accueil</li>
+              </NavLink>
+              <NavLink
+                to="/about"
+                className={(nav) => (nav.isActive ? "nav-active" : "")}
+              >
+                <li>A Propos</li>
+              </NavLink>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
