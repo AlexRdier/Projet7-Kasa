031e1d763ea0ad240e74bc01b1c62738c1f8e18b
Alex Roudier <alexroudier96@gmail.com>
2022-11-01 14:34:28 +0100

    ajout home + about

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+import Header from "../components/Header";
+import { NavLink } from "react-router-dom";
+import { AppContext } from "../AppContext";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  const data = useContext(AppContext);
+
+  return (
+    <div>
+      <main className="main_content">
+        <Header />
+        <Banner location="-home" />
+        <section>
+          <div className="card_container">
+            {data.map((lodging, index) => (
+              <NavLink key={index} to={`/lodging/${lodging.id}`}>
+                <Card key={index} lodging={lodging} />
+              </NavLink>
+            ))}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
